579e8f753ddc060dbf658a7276a5a8da18e350db
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2022-03-13 18:21:38 -0600

    Update api link

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 // eslint-disable-next-line no-console
 console.log(
-  "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me \n\n%cAPI REST en Postman:\nhttps://documenter.getpostman.com/view/9525350/U16krkhd \n\n\n",
+  "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me-api \n\n%cAPI REST en Postman:\nhttps://documenter.getpostman.com/view/9525350/U16krkhd \n\n\n",
   "color:aqua",
   "color:orange"
 );
