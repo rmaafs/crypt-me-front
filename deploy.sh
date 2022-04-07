30280b37ecf075b0496e78dcb7ed83bc19b7cb90
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2022-04-06 20:18:21 -0500

    Restore old method for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #Delete any change
 git reset --hard
 #Change branch with the page builded
-git checkout master
+git checkout gh-pages
 #Bajamos los cambios
 git pull
-#Install dependencies
-npm i
-#Build project
-npm run build
 #Move static page
-cp -a ./build/* /var/www/crypt/
-#Remove build folder
-rm -r ./build
+cp -a ./* /var/www/crypt/
