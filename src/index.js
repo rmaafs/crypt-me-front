a6bc6e4220eb1a99b37dc94f2437aca12314012c
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2023-02-07 19:26:22 -0600

    Using HashRouter with GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 // eslint-disable-next-line no-console
 console.log(
